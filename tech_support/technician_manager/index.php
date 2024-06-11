a861ab7563b0fa8f05201677061d31ca47e60404
Nathaniel <u1133613@umail.usq.edu.au>
2024-06-11 22:51:25 +1000

    Continued technician manager development

--- tech_support/technician_manager/index.php
+++ tech_support/technician_manager/index.php
@@ -4,11 +4,17 @@ require('../model/technician_db.php');
 
 $action = filter_input(INPUT_POST, 'action');
 if ($action === NULL) {
-    // ???
+    $action = filter_input(INPUT_GET, 'action');
+    if ($action === NULL) {
+        $action = 'listTechnicians';
+    }
 }
 
 if ($action == 'listTechnicians') {
-    // ???
+    // Get technician data
+    $technicians = getTechnicians();
+    // Display the technician list
+    include ('technician_list.php');
 } else if ($action == 'deleteTechnician') {
     // ???
 } else if ($action == 'showAddForm') {
--- tech_support/technician_manager/technician_list.php
+++ tech_support/technician_manager/technician_list.php
@@ -13,8 +13,22 @@
             <th>Password</th>
             <th>&nbsp;</th>
         </tr>
-        <?php foreach ($technicians as $technician) : ?>
-             <!--  ??? -->
+        <?php foreach ($technicians as $technician): ?>
+            <tr>
+                <td><?php echo htmlspecialchars($technician['firstName']); ?></td>
+                <td><?php echo htmlspecialchars($technician['lastName']); ?></td>
+                <td><?php echo htmlspecialchars($technician['email']); ?></td>
+                <td><?php echo htmlspecialchars($technician['phone']); ?></td>
+                <td><?php echo htmlspecialchars($technician['password']); ?></td>
+                <td>
+                    <form action="." method="post">
+                        <input type="hidden" name="action" value="deleteTechnician">
+                        <input type="hidden" name="techID"
+                            value="<?php echo htmlspecialchars($technician['techID']); ?>">
+                        <input type="submit" value="Delete">
+                    </form>
+                </td>
+            </tr>
         <?php endforeach; ?>
     </table>
     <p><a href="?action=showAddForm">Add Technician</a></p>
