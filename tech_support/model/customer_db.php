827ccc28faf82ec6ecd84c9f4b24043b7cd71839
Nathaniel <u1133613@umail.usq.edu.au>
2024-06-14 15:04:30 +1000

    Project 3 continuation

--- tech_support/model/customer_db.php
+++ tech_support/model/customer_db.php
@@ -1,6 +1,14 @@
 <?php
 function getCustomers() {
     // ???
+    global $db;
+    $query = 'SELECT * FROM customers
+              ORDER BY customerID';
+    $statement = $db->prepare($query);
+    $statement->execute();
+    $customers = $statement->fetchAll();
+    $statement->closeCursor();
+    return $customers;
 }
 
 function getCustomer_lastName($lastName) {
@@ -9,6 +17,15 @@ function getCustomer_lastName($lastName) {
 
 function getCustomer($customerId) {
     // ???
+    global $db;
+    $query = 'SELECT * FROM customers
+              WHERE customerID = :customerID';
+    $statement = $db->prepare($query);
+    $statement->bindValue(':customerID', $customerId);
+    $statement->execute();
+    $customer = $statement->fetch();
+    $statement->closeCursor();
+    return $customer;
 }
 
 function get_customer_by_email($email) {
@@ -17,17 +34,52 @@ function get_customer_by_email($email) {
 
 function deleteCustomer($customerId) {
     // ???
+    global $db;
+    $query = 'DELETE FROM customers
+              WHERE customerID = :customerID';
+    $statement = $db->prepare($query);
+    $statement->bindValue(':customerID', $customerId);
+    $statement->execute();
+    $statement->closeCursor();
 }
 
-function addCustomer($firstName, $lastName, 
-        $address, $city, $state, $postal_code, $country_code,
+function addCustomer($firstName, $lastName,
         $phone, $email, $password) {
     // ???
+    global $db;
+    $query = 'INSERT INTO customers
+                 (firstName, lastName, phone, email, password)
+              VALUES
+                 (:firstName, :lastName, :phone, :email, :password)';
+    $statement = $db->prepare($query);
+    $statement->bindValue(':firstName', $firstName);
+    $statement->bindValue(':lastName', $lastName);
+    $statement->bindValue(':phone', $phone);
+    $statement->bindValue(':email', $email);
+    $statement->bindValue(':password', $password);
+    $statement->execute();
+    $statement->closeCursor();
 }
 
 function updateCustomer($customerId, $firstName, $lastName,
-        $address, $city, $state, $postal_code, $country_code,
         $phone, $email, $password) {
    // ???
+   global $db;
+    $query = 'UPDATE customers
+              SET firstName = :firstName,
+                  lastName = :lastName,
+                  phone = :phone,
+                  email = :email,
+                  password = :password
+              WHERE customerID = :customerID';
+    $statement = $db->prepare($query);
+    $statement->bindValue(':firstName', $firstName);
+    $statement->bindValue(':lastName', $lastName);
+    $statement->bindValue(':phone', $phone);
+    $statement->bindValue(':email', $email);
+    $statement->bindValue(':password', $password);
+    $statement->bindValue(':customerID', $customerId);
+    $statement->execute();
+    $statement->closeCursor();
 }
 ?>
